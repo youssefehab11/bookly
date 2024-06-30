5c98b7271ea42d8d8bd20599ff89b6f3ff95bf39
youssefehab11 <ucifehab11@gmail.com>
2024-06-30 20:25:43 +0300

    refactor home repo

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly/features/home/data/model/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
-  Either<Failure, List<BookModel>> fetchBestSellerBooks();
-  Either<Failure, List<BookModel>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
